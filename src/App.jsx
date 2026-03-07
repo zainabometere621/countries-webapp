0b427c9a6124bcff871d390e1d838d3d62b46507
Zainab Ometere <zainabometere621@gmail.com>
2026-03-07 12:51:34 +0100

    feat: I added outlet

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import LandingPage from "./pages/LadingPage";
 import AllCountriesPage from "./pages/AllCountriesPage";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/countries" element={<AllCountriesPage />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/countries" element={<AllCountriesPage />} />
+      </Route>
     </Routes>
   );
 }
--- src/pages/LadingPage.jsx
+++ src/pages/LadingPage.jsx
@@ -1,4 +1,3 @@
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import KeyFeatures from "../components/KeyFeatures";
@@ -6,7 +5,6 @@ import KeyFeatures from "../components/KeyFeatures";
 export default function LandingPage() {
   return (
     <div className="bg-blue-50">
-      <Navbar />
       <Hero />
       <About />
       <KeyFeatures />
