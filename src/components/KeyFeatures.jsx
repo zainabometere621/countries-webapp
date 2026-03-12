1b0e042d6086630943e9f48569951e2cef8817b6
Zainab Ometere <zainabometere621@gmail.com>
2026-03-12 16:08:06 +0100

    I worked on the specific country page

--- src/components/KeyFeatures.jsx
+++ src/components/KeyFeatures.jsx
@@ -1,28 +1,3 @@
-// import {keyFeaturesCard} from "../data"
-// export default function KeyFeatures() {
-//   return (
-//     <section className="flex flex-col items-center justify-center mt-24 mb-10 space-y-6">
-//       <h1 className="font-bold text-3xl">Key Features</h1>
-
-//       {/* Single grid container */}
-//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-32 pb-20">
-//         {keyFeaturesCard.map((card, index) => (
-//           <div
-//             key={index}
-//             className="p-6 bg-gray-50 border border-gray-200 rounded-xl shadow-sm space-y-4"
-//           >
-//             <div className="w-14 h-14 bg-blue-100 flex items-center justify-center rounded-lg">
-//               <MapPin className="text-blue-700 w-8 h-8" />
-//             </div>
-//             <h2 className="font-semibold text-xl">{card.title}</h2>
-//             <p className="text-gray-600 text-base">{card.description}</p>
-//           </div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// }
-
 import {keyFeaturesCard} from "../data"
 export default function KeyFeatures() {
   return (
--- src/pages/specificCountryPage.jsx
+++ src/pages/specificCountryPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams,NavLink } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 import { ArrowLeft } from "lucide-react";
 
 export default function SpecificCountryPage() {
@@ -17,25 +17,42 @@ export default function SpecificCountryPage() {
       } catch (error) {
         console.log(error);
       }
-      
     }
     fetchData();
   }, [params.countryCode]);
   // console.log(countries)
   return (
     <main className=" px-6 py-10 bg-gray-100 ">
-      <NavLink to = "/countries" className=" flex items-center text-base space-x-4 text-center font-medium px-3 py-2 rounded-lg w-fit hover:bg-gray-200">
+      <NavLink
+        to="/countries"
+        className=" flex items-center text-base space-x-4 text-center font-medium px-3 py-2 rounded-lg w-fit hover:bg-gray-200"
+      >
         <ArrowLeft className="h-4 w-4" />
         <p>Back to Countries</p>
       </NavLink>
 
-      <section className="grid grid-cols-4 gap-4 max-w-6xl mx-auto my-6">
+      <section className="">
         {country.map((c) => (
-          <div key={c.cca2} className="p-2 border border-gray-200 text-md">
-            <p>{c.name.common}</p>
-            <p>{c.flag}</p>
-            <p>{c.currencies ? Object.values(c.currencies)[0].name : "N/A"}</p>
+          <div
+            key={c.cca2}
+            className="p-2 bg-white text-md p-8 rounded-lg shadow-lg flex space-x-6 "
+          >
+            {/* <p>{c.name.common}</p> */}
+            <img
+              src={c.flags.png}
+              alt={c.name.common}
+              className="h-40 w-36 rounded-lg"
+            />
+            {/* <p>{c.currencies ? Object.values(c.currencies)[0].name : "N/A"}</p> */}
             {/* <p>{c.website}</p> */}
+            <div className="flex flex-col space-y-4 pt-2">
+              <p className="font-semibold text-5xl">{c.name.common}</p>
+              <p className="text-gray-600 text-5xl">{c.capital}</p>
+              <span className="flex space-x-4">
+                <p className="px-6 py-1 rounded-3xl text-sm bg-blue-100 text-blue-700 text-sm ">{c.region}</p>
+                <p className="px-6 py-1 rounded-3xl text-sm bg-green-100 text-green-700">{c.subregion}</p>
+              </span>
+            </div>
           </div>
         ))}
       </section>
