4455dbd6f141db463012a75d04be43cca7aeb4c5
Zainab Ometere <zainabometere621@gmail.com>
2026-03-13 14:02:03 +0100

    feat: integrating api into my specific country page

--- src/pages/specificCountryPage.jsx
+++ src/pages/specificCountryPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, NavLink } from "react-router-dom";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, MapPin } from "lucide-react";
+import logo1 from "/src/assets/blue-logoss.png";
 
 export default function SpecificCountryPage() {
   const [country, setCountry] = useState([]);
@@ -25,7 +26,7 @@ export default function SpecificCountryPage() {
     <main className=" px-6 py-10 bg-gray-100 ">
       <NavLink
         to="/countries"
-        className=" flex items-center text-base space-x-4 text-center font-medium px-3 py-2 rounded-lg w-fit hover:bg-gray-200"
+        className=" flex items-center text-base space-x-4 text-center my-6 font-medium px-3 py-2 rounded-lg w-fit hover:bg-gray-200"
       >
         <ArrowLeft className="h-4 w-4" />
         <p>Back to Countries</p>
@@ -33,25 +34,85 @@ export default function SpecificCountryPage() {
 
       <section className="">
         {country.map((c) => (
-          <div
-            key={c.cca2}
-            className="p-2 bg-white text-md p-8 rounded-lg shadow-lg flex space-x-6 "
-          >
-            {/* <p>{c.name.common}</p> */}
-            <img
-              src={c.flags.png}
-              alt={c.name.common}
-              className="h-40 w-36 rounded-lg"
-            />
-            {/* <p>{c.currencies ? Object.values(c.currencies)[0].name : "N/A"}</p> */}
-            {/* <p>{c.website}</p> */}
-            <div className="flex flex-col space-y-4 pt-2">
-              <p className="font-semibold text-5xl">{c.name.common}</p>
-              <p className="text-gray-600 text-5xl">{c.capital}</p>
-              <span className="flex space-x-4">
-                <p className="px-6 py-1 rounded-3xl text-sm bg-blue-100 text-blue-700 text-sm ">{c.region}</p>
-                <p className="px-6 py-1 rounded-3xl text-sm bg-green-100 text-green-700">{c.subregion}</p>
-              </span>
+          <div key={c.cca2} className="flex flex-col space-y-6">
+            <div className="p-2 bg-white text-md p-8 rounded-lg shadow-lg flex space-x-6 ">
+              {/* <p>{c.name.common}</p> */}
+              <img
+                src={c.flags.png}
+                alt={c.name.common}
+                className="h-40 w-36 rounded-lg"
+              />
+              {/* <p>{c.currencies ? Object.values(c.currencies)[0].name : "N/A"}</p> */}
+              {/* <p>{c.website}</p> */}
+              <div className="flex flex-col space-y-4 pt-2">
+                <p className="font-semibold text-5xl">{c.name.common}</p>
+                <p className="text-gray-600 text-5xl">{c.capital}</p>
+                <span className="flex space-x-4">
+                  <p className="px-6 py-1 rounded-3xl text-sm bg-blue-100 text-blue-700 text-sm ">
+                    {c.region}
+                  </p>
+                  <p className="px-6 py-1 rounded-3xl text-sm bg-green-100 text-green-700">
+                    {c.subregion}
+                  </p>
+                </span>
+              </div>
+            </div>
+            <div className="flex space-x-4">
+              <div className="p-2 bg-white text-md p-8 rounded-lg pb-24 shadow-lg w-full space-y-6">
+                <span className="flex space-x-2 items-center">
+                  <img src={logo1} alt="Logo" className="h-10 w-10" />
+                  <p className="font-semibold text-xl">General Information</p>
+                </span>
+                <div>
+                  <p className="flex itmes-center justify-between">
+                    <span className="text-gray-400 text-base">Capital:</span>
+                    <span className="font-semibold text-base">{c.capital}</span>
+                  </p>
+                  <hr />
+                </div>
+                <div>
+                  <span className="flex itmes-center justify-between">
+                    <p className="text-gray-400 text-base">Population:</p>
+                    <p className="font-semibold text-base">{c.population}</p>
+                  </span>
+                  <hr />
+                </div>
+                <div>
+                  <span className="flex itmes-center justify-between">
+                    <p className="text-gray-400 text-base">Area:</p>
+                    <p className="font-semibold text-base">{c.area}</p>
+                  </span>
+                  <hr />
+                </div>
+                <div>
+                  <span className="flex itmes-center justify-between">
+                    <p className="text-gray-400 text-base">Time Zone:</p>
+                    <span className="font-semibold text-base">{c.timezones}</span>
+                  </span>
+                  <hr />
+                </div>
+                <div>
+                  <span className="flex itmes-center justify-between">
+                    <p className="text-gray-400 text-base">Status:</p>
+                    <span className="font-semibold text-base">{c.status}</span>
+                  </span>
+                  <hr />
+                </div>
+              </div>
+              <div className="p-2 bg-white text-md p-8 rounded-lg shadow-lg w-full">
+                <span className="flex space-x-2 items-center">
+                  <MapPin className="h-6 w-6 text-green-700" />
+                  <p className="font-semibold text-xl">Geographic data</p>
+                </span>
+                
+                <div>
+                  <span className="flex itmes-center justify-between">
+                    <p className="text-gray-400 text-base">Time Zone:</p>
+                    <span className="font-semibold text-base">{c.timezones}</span>
+                  </span>
+                  <hr />
+                </div>
+              </div>
             </div>
           </div>
         ))}
