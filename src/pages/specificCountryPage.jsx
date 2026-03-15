6083bb53e0f3cdc687db43e30177f761bfab4b36
Zainab Ometere <zainabometere621@gmail.com>
2026-03-15 15:35:29 +0100

    I added responsivesness to specific country page

--- src/pages/specificCountryPage.jsx
+++ src/pages/specificCountryPage.jsx
@@ -34,20 +34,26 @@ export default function SpecificCountryPage() {
 
       <section className="">
         {country.map((c) => (
-          <div key={c.cca2} className="flex flex-col space-y-6">
-            <div className="p-2 bg-white text-md p-8 rounded-lg shadow-lg flex space-x-6 ">
+          <div key={c.cca2} className="flex flex-col space-y-4 lg:space-y-6">
+            <div className="p-2 bg-white text-md px-6 py-8 lg:p-8 rounded-lg shadow-lg flex flex-col  lg:flex-row space-x-6 ">
               {/* <p>{c.name.common}</p> */}
               <img
                 src={c.flags.png}
                 alt={c.name.common}
-                className="h-40 w-36 rounded-lg"
+                className="h-40 w-full lg:w-36 rounded-lg"
               />
               {/* <p>{c.currencies ? Object.values(c.currencies)[0].name : "N/A"}</p> */}
               {/* <p>{c.website}</p> */}
               <div className="flex flex-col space-y-4 pt-2">
-                <p className="font-semibold text-5xl">{c.name.common}</p>
-                <p className="text-gray-600 text-5xl">{c.capital}</p>
-                <span className="flex space-x-4">
+                <div className="flex flex-row items-center justify-between lg:flex-col lg:items-start lg:justify-start">
+                  <p className="font-semibold text-2xl lg:text-4xl">
+                    {c.name.common}
+                  </p>
+                  <p className="text-gray-600 text-2xl lg:text-4xl">
+                    {c.capital}
+                  </p>
+                </div>
+                <span className="flex items-center justify-between space-x-4">
                   <p className="px-6 py-1 rounded-3xl text-sm bg-blue-100 text-blue-700 text-sm ">
                     {c.region}
                   </p>
@@ -57,37 +63,51 @@ export default function SpecificCountryPage() {
                 </span>
               </div>
             </div>
-            <div className="flex space-x-4">
-              <div className="p-2 bg-white text-md p-8 rounded-lg pb-24 shadow-lg w-full space-y-6">
+            <div className="grid grid-cols-1 lg:grid-cols-2 space-y-4 lg:pr-4 lg:space-y-0 lg:space-x-4">
+              <div className=" bg-white text-md p-8 rounded-lg pb-24 shadow-lg w-full space-y-6">
                 <span className="flex space-x-2 items-center">
                   <img src={logo1} alt="Logo" className="h-10 w-10" />
-                  <p className="font-semibold text-xl">General Information</p>
+                  <p className="font-semibold text-lg lg:text-xl">
+                    General Information
+                  </p>
                 </span>
                 <div>
                   <p className="flex itmes-center justify-between">
-                    <span className="text-gray-400 text-base">Capital:</span>
-                    <span className="font-semibold text-base">{c.capital}</span>
+                    <span className="text-gray-400 text-sm lg:text-base">
+                      Capital:
+                    </span>
+                    <span className="font-semibold text-sm lg:text-base">
+                      {c.capital}
+                    </span>
                   </p>
                   <hr />
                 </div>
                 <div>
                   <span className="flex itmes-center justify-between">
-                    <p className="text-gray-400 text-base">Population:</p>
-                    <p className="font-semibold text-base">{c.population}</p>
+                    <p className="text-gray-400 text-sm lg:text-base">
+                      Population:
+                    </p>
+                    <p className="font-semibold text-sm lg:text-base">
+                      {c.population}
+                    </p>
                   </span>
                   <hr />
                 </div>
                 <div>
                   <span className="flex itmes-center justify-between">
-                    <p className="text-gray-400 text-base">Area:</p>
-                    <p className="font-semibold text-base">{c.area}</p>
+                    <p className="text-gray-400 text-sm lg:text-base">Area:</p>
+                    <p className="font-semibold text-sm lg:text-base">
+                      {c.area}
+                    </p>
                   </span>
                   <hr />
                 </div>
                 <div>
                   <span className="flex itmes-center justify-between">
-                    <p className="text-gray-400 text-base">Time Zone:</p>
-                    <span className="font-semibold text-base">
+                    <p className="text-gray-400 text-sm lg:text-base">
+                      Time Zone:
+                    </p>
+                    <span className="font-semibold text-sm lg:text-base">
                       {c.timezones}
                     </span>
                   </span>
@@ -95,8 +115,12 @@ export default function SpecificCountryPage() {
                 </div>
                 <div>
                   <span className="flex itmes-center justify-between">
-                    <p className="text-gray-400 text-base">Status:</p>
-                    <span className="font-semibold text-base">{c.status}</span>
+                    <p className="text-gray-400 text-sm lg:text-base">
+                      Status:
+                    </p>
+                    <span className="font-semibold text-sm lg:text-base">
+                      {c.status}
+                    </span>
                   </span>
                   <hr />
                 </div>
@@ -104,28 +128,40 @@ export default function SpecificCountryPage() {
               <div className="p-2 bg-white text-md p-8 rounded-lg space-y-6 shadow-lg w-full">
                 <span className="flex space-x-2 items-center">
                   <MapPin className="h-6 w-6 text-green-700" />
-                  <p className="font-semibold text-xl">Geographic data</p>
+                  <p className="font-semibold text-lg lg:text-xl">
+                    Geographic data
+                  </p>
                 </span>
                 <div className="flex flex-col gap-2">
-                  <p className="text-gray-400 text-base">Region:</p>
-                  <span className="font-semibold text-base">{c.region}</span>
+                  <p className="text-gray-400 text-sm lg:text-base">Region:</p>
+                  <span className="font-semibold text-sm lg:text-base">
+                    {c.region}
+                  </span>
                   <hr />
                 </div>
                 <div className="flex flex-col gap-2">
-                  <p className="text-gray-400 text-base">SubRegion:</p>
-                  <span className="font-semibold text-base">{c.subregion}</span>
+                  <p className="text-gray-400 text-sm lg:text-base">
+                    SubRegion:
+                  </p>
+                  <span className="font-semibold text-sm lg:text-base">
+                    {c.subregion}
+                  </span>
                   <hr />
                 </div>
                 <div className="flex flex-col gap-2">
-                  <p className="text-gray-400 text-base">Languages:</p>
-                  <span className="font-semibold text-base">
+                  <p className="text-gray-400 text-sm lg:text-base">
+                    Languages:
+                  </p>
+                  <span className="font-semibold text-sm lg:text-base">
                     {Object.values(c.languages ?? {}).join(", ")}
                   </span>
                   <hr />
                 </div>
                 <div className="flex flex-col gap-2">
-                  <p className="text-gray-400 text-base">Currencies:</p>
-                  <span className="font-semibold text-base">
+                  <p className="text-gray-400 text-sm lg:text-base">
+                    Currencies:
+                  </p>
+                  <span className="font-semibold text-sm lg:text-base">
                     {Object.values(c.currencies ?? {})
                       .map((currency) => currency.name)
                       .join(", ")}
